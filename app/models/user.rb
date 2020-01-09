73640c8cd072280aad73618d0ef493c20c0dd739
鈴木 健一朗 <superken4396@gmail.com>
2020-01-09 10:26:50 +0000

    課題

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
+  
+  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  
+#パスワードをアルファベット、数字の混合のみ可
+  validates :password, format: { with: VALID_PASSWORD_REGEX}
+#８〜３２文字制限
+  validates :password, length: { minimum: 8, maximum: 32}
+  
+#メールアドレスの正規表現
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+#15文字まで許可するようにバリデーションを設定
+  validates :name, length: { maximum: 15}
+  
+  has_secure_password
 end
